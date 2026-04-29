30512ea4ed0518248ef3d410091ac73af2aa609f
731096 <731096@student.dusd.net>
2026-04-29 15:27:31 -0700

    Mechanics
    ENEMY

--- P7LockwoodUnit4/Assets/Scripts/PlayerController.cs
+++ P7LockwoodUnit4/Assets/Scripts/PlayerController.cs
@@ -22,6 +22,7 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -29,8 +30,10 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("Powerup"))
         {
             hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
             StartCoroutine(PowerupCountdownRoutine());
+
             
         
         }
@@ -39,6 +42,7 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(7);
         hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
 
     }
     private void OnCollisionEnter(Collision collision)
--- P7LockwoodUnit4/Assets/Scripts/SpawnManager.cs
+++ P7LockwoodUnit4/Assets/Scripts/SpawnManager.cs
@@ -4,16 +4,26 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private float spawnRange = 9.0f;
+    public int enemyCount;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(3);
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
-        
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length;
+        if (enemyCount == 0)
+        {
+            SpawnEnemyWave(1);
+        }
+    }
+    void SpawnEnemyWave(int emeiesToSpawn)
+    {
+        for (int i = 0; i < emeiesToSpawn; i++)
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
     }
     private Vector3 GenerateSpawnPosition() 
     {
